10785a01d283f3cc7ace536c659290ef805a2e98
CristianoAltomonte <cristianoaltomonte@hotmail.it>
2022-11-02 10:24:04 +0000

    correzioni varie

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,41 +1,36 @@
 const griglia = document.getElementById('griglia');
-console.log(griglia);
-
+const bottone = document.getElementById("bottone");
 
 // CREAZIONE DI UN DIV DI FORMA QUADRATA DENTRO LA GRIGLIA
 
-function play(){
+function creazioneQuadrato(num) {
 
-    function creazioneQuadrato() {
+    const div = document.createElement('div');
+    div.classList.add('quadrato');
+    div.innerHTML = num;
 
-        const div = document.createElement('div');
-        div.classList.add('quadrato');
-        
-        return div;
-    }
-    console.log(creazioneQuadrato());
-    
-    // inseriamo il quadrato creato dalla funzione nella griglia
-    // griglia.append(creazioneQuadrato());
-    
-    // creazione dei 64 quadratini 
-    
-    for (let i = 1; i <= 100; i++) {
-    
-        let elementoCorrente = creazioneQuadrato(i);
-        console.log(elementoCorrente);
-        elementoCorrente.innerText += (i)
-        elementoCorrente.addEventListener('click', function () { 
-            console.log(this);
-            this.classList.toggle ('active');
-       
-        })
-        
-        griglia.append(elementoCorrente);
-    
-    
-    }
-
+    return div;
 }
 
+// inseriamo il quadrato creato dalla funzione nella griglia
+// griglia.append(creazioneQuadrato());
 
+// creazione dei 100 quadratini 
+
+bottone.addEventListener('click', function () {
+
+    for (let i = 1; i <= 100; i++) {
+        let elementoCorrente = creazioneQuadrato(i);
+
+        elementoCorrente.addEventListener('click', function () {
+            this.classList.toggle('active');
+        })
+
+        elementoCorrente.innerHTML = i;
+        elementoCorrente.classList.add("text-center", "align-items-center");
+
+
+        griglia.append(elementoCorrente);
+    }
+
+}, { once: true });
